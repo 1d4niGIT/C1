8a57855c18b46df224f1758f4ac0834a60cab5d3
cristianpuris-bit <cristian.puris@tecsup.edu.pe>
2026-08-18 16:37:46 -0500

    mov

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using static T1;
+
+public class lab : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player reptile = new Player();
+        reptile.Name = "Reptile";
+        reptile.atk = 10;
+        reptile.vida = 100;
+        reptile.veloc = 150;
+        reptile.Type = Razas.reptil;
+
+        reptile.introduction();
+    }
+    public enum Razas
+    {
+        orco,
+        reptil,
+        minotauro,
+        soul
+
+    }
+    public class Player
+    {
+        public string Name;
+        public int atk;
+        public int vida;
+        public int veloc;
+        public Razas Type;
+
+
+        public void introduction()
+        {
+            Debug.Log("soy" + Name
+                + "\n Mis puntos de ataque son: " + atk
+                + "\n Mis puntos de defensa son: " + vida
+                + "\n Soy un : " + Type.ToString());
+        }
+
+
+    }
+
+}
